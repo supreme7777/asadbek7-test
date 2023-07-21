39e4159659b0b8a77053225a6350de8976affa76
supreme7777 <asadbekraxmatullaev0070@gmail.com>
2023-07-21 16:20:29 +0500

    all

---
+++ loader.js
@@ -0,0 +1,7 @@
+// window.onload =function (){
+//     loader.getElementsByClassName.
+// };
+let loader =document.getElementById('loader');
+window.addEventListener('load', function(){
+loader.style.display = "none"
+})
